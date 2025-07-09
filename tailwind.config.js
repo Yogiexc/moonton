4a9383f48dd6d467438b2329cff27d181eac8b27
Yogiexc <yogiexsaputra@gmail.com>
2025-07-09 16:54:19 +0700

    feat:init commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,23 @@ module.exports = {
         './resources/js/**/*.js',
     ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            theme: {
+                fontFamily: {
+                    poppins: 'Poppins, sans-serif'
             },
-        },
-    },
+            extend: {
+                colors: {
+                    "alerange": '#FB6908',
+                    "gray-1": '#B4B4B4',
+                    "gray-2": '#E2E0E0',
+                    "form-bg": '#212121',
+            },
+             screens: {
+                    'laptopLg': '1160px',
+                    'laptopXl': '1360px',
+                },
+            },
+        },    
 
     plugins: [require('@tailwindcss/forms')],
 };
