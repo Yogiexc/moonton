7d3ab3a034e9ee80d9f4d28e1a9e81900403dff4
Yogiexc <yogiexsaputra@gmail.com>
2025-09-01 13:54:25 +0700

    feat: slicing feature movie

--- resources/js/Pages/Prototype/Movie/Show.js
+++ resources/js/Pages/Prototype/Movie/Show.js
@@ -8,8 +8,8 @@ export default function Show(){
         <ReactPlayer 
             url="https://www.youtube.com/watch?v=a6aNvKntetY"
             controls
-            width={"100%"}
-            height={"850px"}
+            width="100%"
+            height="850px"
 />
     </div>
 
