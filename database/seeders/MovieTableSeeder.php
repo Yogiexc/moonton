fddb311024467c7773811b58ac8db4beaab9dc86
Yogiexc <yogiexsaputra@gmail.com>
2025-09-08 12:17:23 +0700

    feat: integration dashboard

--- database/seeders/MovieTableSeeder.php
+++ database/seeders/MovieTableSeeder.php
@@ -16,34 +16,34 @@ class MovieTableSeeder extends Seeder
     public function run()
     {
         $movies = [
-            [
-                'name' => 'The Shawshank Redemption',
-                'slug' => 'the-shawshank-redemption',
-                'category' => 'Drama',
-                'vidio_url' => 'https://example.com/shawshank',
-                'thumbnail' => 'https://example.com/thumbnails/shawshank.jpg',
-                'rating' => 9.3,
-                'is_featured' => 1,
-            ],
-            [
-                'name' => 'The Godfather',
-                'slug' => 'the-godfather',
-                'category' => 'Crime',
-                'vidio_url' => 'https://example.com/godfather',
-                'thumbnail' => 'https://example.com/thumbnails/godfather.jpg',
-                'rating' => 9.2,
-                'is_featured' => 0,
-            ],
-            [
-                'name' => 'The Dark Knight',
-                'slug' => 'the-dark-knight',
-                'category' => 'Action',
-                'vidio_url' => 'https://example.com/dark-knight',
-                'thumbnail' => 'https://example.com/thumbnails/dark-knight.jpg',
-                'rating' => 9.0,
-                'is_featured' => 0,
-            ],
-        ];
+    [
+        'name' => 'The Shawshank Redemption',
+        'slug' => 'the-shawshank-redemption',
+        'category' => 'Drama',
+        'vidio_url' => 'https://www.youtube.com/watch?v=6hB3S9bIaco',
+        'thumbnail' => 'https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
+        'rating' => 4.3,
+        'is_featured' => 1,
+    ],
+    [
+        'name' => 'The Godfather',
+        'slug' => 'the-godfather',
+        'category' => 'Crime',
+        'vidio_url' => 'https://www.youtube.com/watch?v=sY1S34973zA',
+        'thumbnail' => 'https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg',
+        'rating' => 4.2,
+        'is_featured' => 0,
+    ],
+    [
+        'name' => 'The Dark Knight',
+        'slug' => 'the-dark-knight',
+        'category' => 'Action',
+        'vidio_url' => 'https://www.youtube.com/watch?v=EXeTwQWrcwY',
+        'thumbnail' => 'https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg',
+        'rating' => 4.0,
+        'is_featured' => 0,
+    ],
+];
         Movie::insert($movies);
     }
 }
---
+++ resources/js/Pages/User/Dashboard/Index.js
@@ -0,0 +1,60 @@
+import Authenticated from "@/Layouts/Authenticated/Index";
+import {Head} from "@inertiajs/inertia-react";
+import Flickity from "react-flickity-component";
+import FeaturedMovie from "@/Components/FeaturedMovie";
+import MovieCard from "@/Components/MovieCard";
+
+export default function Dashboard({ auth, featuredMovies, movies }) {
+    const flickityOption = {
+        "cellAlign": "left",
+        "contain": true,
+        "groupCells": 1,
+        "wrapAround": false,
+        "pageDots": false,
+        "prevNextButtons": false,
+        "draggable": ">1"
+    };
+    return (
+        <Authenticated auth={auth}>
+        <Head>
+            <link 
+            rel="stylesheet" 
+            href="https://unpkg.com/flickity@2/dist/flickity.min.css" 
+            />
+            <title>Dashboard</title>       
+        </Head>  
+            <div>
+                <div className="font-semibold text-[22px] text-black mb-4">
+                    Featured Movies
+                </div>
+                
+                <Flickity className="gap-[30px]" options={flickityOption}>
+                    {featuredMovies.map((featuredMovies) => (
+                        <FeaturedMovie 
+                        key={featuredMovies.id} 
+                        slug={featuredMovies.slug} 
+                        name={featuredMovies.name} 
+                        category={featuredMovies.category} 
+                        thumbnail={featuredMovies.thumbnail}
+                        rating={featuredMovies.rating}
+                        />  
+                    ))}
+                </Flickity>
+            </div>
+            <div className="mt-[50px]">
+                    <div className="font-semibold text-[22px] text-black mb-4">Browse</div>
+                    <Flickity className="gap-[30px]" options={flickityOption}>
+                    {movies.map((movie) => (
+                        <MovieCard 
+                        key={movie.id}
+                        slug={movie.slug} 
+                        name={movie.name}
+                        category={movie.category}
+                        thumbnail={movie.thumbnail}
+                        />  
+                    ))}
+                    </Flickity>
+                </div>
+        </Authenticated>
+    );
+}
